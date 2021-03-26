371056217ef52f1380074b50fff458acde50cba8
Hamilkar247 <adonnibal96@gmail.com>
2021-03-26 16:25:52 +0100

    [#1][CU-1egxw] wydzieliłem funkcję getFile Request.py
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- usim800/Request/Request.py
+++ usim800/Request/Request.py
@@ -94,6 +94,45 @@ class request(communicate):
 
         return self._status_code
 
+    def getFile(self, url, header=None):
+        self.init()
+        self._url = url
+        self._IP = self._bearer(self._APN)
+
+        cmd = "AT + HTTPINIT"
+        self._send_cmd(cmd)
+        cmd = 'AT + HTTPPARA="CID",1'
+        self._send_cmd(cmd)
+
+        cmd = 'AT + HTTPPARA="URL","{}"'.format(url)
+        self._send_cmd(cmd)
+        time.sleep(3)
+        cmd = "AT +HTTPACTION=0"
+
+        self._send_cmd(cmd)
+        time.sleep(2)
+        cmd = "AT +HTTPREAD"
+        self._send_cmd(cmd, get_decode_data=True)
+        data = self._getdata(
+            data_to_decode=[], string_to_decode=None, till=b'\n', count=2, counter=0)
+        tk = Parser(data)
+        token = tk.tokenizer()
+        self._content = tk.parser
+        if (len(token) == 4):
+            self._status_code = token[2]
+            read_bytes = token[3]
+            string = self._read_sent_data(int(read_bytes)+1000)
+            tk = Parser(string)
+
+            self._content = tk.bytesparser
+            self._text = tk.parser
+            jph = JsonParser.ATJSONObjectParser(string)
+            self._json = jph.JSONObject
+        cmd = "AT +SAPBR=0,1"
+        self._send_cmd(cmd)
+
+        return self._status_code
+
     def getConfig(self, url, header=None):
         print("getConfig")
         self.init()
