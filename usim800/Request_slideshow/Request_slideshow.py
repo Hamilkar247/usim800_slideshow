a636aca7085c1af58fc9e9864c02425a1f7ab58a
Hamilkar247 <adonnibal96@gmail.com>
2021-04-06 18:38:06 +0200

    [#2] Request_slideshow - metody inicjalne
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- usim800/Request_slideshow/Request_slideshow.py
+++ usim800/Request_slideshow/Request_slideshow.py
@@ -1,7 +1,23 @@
 from usim800.Communicate_slideshow import communicate_slideshow
+import logging
 
 
 class request_slideshow(communicate_slideshow):
 
     def __init__(self, *args, **kwargs):
-        pass
+        super().__init__(*args, **kwargs)
+
+        self._status_code = None
+        self._json = None
+        self._text = None
+        self._content = None
+        self._url = None
+        self._IP = None
+
+    def init(self):
+        self._status_code = None
+        self._json = None
+        self._text = None
+        self._content = None
+        self._url = None
+        self._IP = None
