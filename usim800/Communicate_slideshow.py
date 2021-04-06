2cac82ae4bf77d756c856c1365bb28e79d820264
Hamilkar247 <adonnibal96@gmail.com>
2021-04-06 13:15:35 +0200

    [#2] Communicate_slideshow - readtill - odczytywanie do momentu
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- usim800/Communicate_slideshow.py
+++ usim800/Communicate_slideshow.py
@@ -1,12 +1,20 @@
 import logging
+import serial
 
-
-class communicate:
+class communicate_slideshow:
     cmd_list = []
 
     def __init__(self, port):
         self._port = port
+        self._port=0
 
     def _setcmd(self, cmd, end='\r\n'):
         end = '\r\n'
         return (cmd + end)
+
+    def _readtill(self, till="OK"):
+        receive = self._port.read(14816)      #ta liczba to się wydaje że to liczba bajtów
+        receive_decode= receive.decode()
+        while "OK" not in receive_decode:
+            receive_decode += receive_decode.decode()
+            receive = self._port.read(14816)
