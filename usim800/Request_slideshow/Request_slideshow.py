edecdf3389f0fe58f4515b10eedeff7ba9cd92e7
Hamilkar247 <adonnibal96@gmail.com>
2021-04-06 19:41:49 +0200

    [#2] Request_slideshow - metoda getFile
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- usim800/Request_slideshow/Request_slideshow.py
+++ usim800/Request_slideshow/Request_slideshow.py
@@ -1,5 +1,6 @@
 from usim800.Communicate_slideshow import communicate_slideshow
 import logging
+import time
 
 
 class request_slideshow(communicate_slideshow):
@@ -21,3 +22,26 @@ class request_slideshow(communicate_slideshow):
         self._content = None
         self._url = None
         self._IP = None
+
+    def getFile(self, url, header=None):
+        self.init()
+        self._url = url
+        self._IP = self._bearer(self._APN)
+
+        #inicjalizacja połączenia HTTP
+        cmd = 'AT+HTTPINIT'
+        self._send_cmd(cmd)
+        cmd = 'AT+HTTPPARA="CID", 1'
+        self._send_cmd(cmd)
+        cmd = 'AT+HTTPARA="URL", "{}"'.format(url)
+        self._send_cmd(cmd)
+        time.sleep(3)
+        cmd = 'AT+HTTPACTION=0'
+        self._send_cmd(cmd)
+        time.sleep(2)
+        cmd = "AT+HTTPREAD"
+        self._send_cmd(cmd, get_decode_data=True)
+        data = self._getdata(
+            data_to_decode=[], string_to_decode=None, till=b'\n', count=2, counter=0)
+        #tk = ParserFile(data)
+
