037a44ebad78cf8b6601718eec9b13ba538f20b8
Hamilkar247 <adonnibal96@gmail.com>
2021-04-06 18:21:43 +0200

    [#2] Request_slideshow - dodanie w usim800_slideshow
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ usim800/Request_slideshow/Request_slideshow.py
@@ -0,0 +1,7 @@
+from usim800.Communicate_slideshow import communicate_slideshow
+
+
+class request_slideshow(communicate_slideshow):
+
+    def __init__(self, *args, **kwargs):
+        pass
---
+++ usim800/Request_slideshow/__init__.py
@@ -0,0 +1,1 @@
+from usim800.Request_slideshow.Request_slideshow import request_slideshow
