a8135c0052bdbf27ceec754a695181a7dbcab32b
Hamilkar247 <adonnibal96@gmail.com>
2021-04-12 15:54:39 +0200

    [#2] poprawne pobieranie pliku
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- usim800/Request_slideshow/Request_slideshow.py
+++ usim800/Request_slideshow/Request_slideshow.py
@@ -14,6 +14,7 @@ class request_slideshow(communicate_slideshow):
         self._status_code = None
         self._numberOfBytes = None
         self._sleep_to_read_bytes = None
+        self._nameOfFile = None
         self._json = None
         self._text = None
         self._content = None
@@ -24,6 +25,7 @@ class request_slideshow(communicate_slideshow):
         self._status_code = None
         self._numberOfBytes = None
         self._sleep_to_read_bytes = None
+        self._nameOfFile = None
         self._json = None
         self._text = None
         self._content = None
@@ -58,11 +60,20 @@ class request_slideshow(communicate_slideshow):
     def status_code(self):
         return self._status_code
 
-    def getFile(self, url, sleep_to_read_bytes, header=None):
+    @property
+    def numberOfBytes(self):
+        return self._numberOfBytes
+
+    @property
+    def nameOfFile(self):
+        return self._nameOfFile
+
+    def getFile(self, url, sleep_to_read_bytes, nameOfFile):
         logging.debug("Jestem w getFile")
         self.init()
         self._url = url
         self._sleep_to_read_bytes = sleep_to_read_bytes
+        self._nameOfFile = nameOfFile
         try:
           self._IP = self._bearer(self._APN)
         except Exception as e:
@@ -71,20 +82,15 @@ class request_slideshow(communicate_slideshow):
         #inicjalizacja połączenia HTTP
         cmd = 'AT+HTTPINIT'
         self._send_cmd(cmd)
-        #cmd = 'AT+HTTPPARA="CID", 1'
-        #self._send_cmd(cmd)
         cmd = f'AT+HTTPPARA="URL","{url}"'
         self._send_cmd(cmd)
         #ustawia nam _status_code i _number_of_bytes
         self.parserHTTPACTION(cmd)
         try:
-            file_bytes=self.receiveHTTTPREAD(cmd)
-            print(file_bytes)
+            self.receiveHTTTPREAD(cmd)
         except Exception as e:
             print("Wystapil blad przy odbieraniu danych")
             print(f"tresc błędu {e}")
-            file_bytes=None
-        return file_bytes
 
     def parserHTTPACTION(self, cmd):
         try:
@@ -106,21 +112,16 @@ class request_slideshow(communicate_slideshow):
             traceback.print_exc()
 
     def receiveHTTTPREAD(self, cmd):
+        logging.debug("receiveHTTPREAD - method")
         try:
             time.sleep(1)
-            cmd = 'AT+HTTPREAD'
-            bytes_file_and_at_com=self._send_cmd(cmd, get_decode_data=False, return_data=True, t=self._sleep_to_read_bytes)
-            logging.debug(f"pobrane {bytes_file_and_at_com}")
-            logging.debug(f"typ {type(bytes_file_and_at_com)}")
-            logging.debug(f"podzielone")
-            #przykladowo efekt bedzie b'2729'+b'\r\n'
-            splitter=b"HTTPREAD: "+self._numberOfBytes+b"\r\n"
-            logging.debug(bytes_file_and_at_com.split(splitter))
-            only_bytes_file=bytes_file_and_at_com.split(splitter)[1]
-            logging.debug(sys.getsizeof(only_bytes_file))
-            return only_bytes_file
+            logging.debug(f"receive HTTPREAD")
+            cmd = "AT +HTTPREAD"
+            self._send_cmd_and_save_answer(cmd, t=self._sleep_to_read_bytes, size=self._numberOfBytes
+                                           , nameSaveFile=self._nameOfFile)
         except Exception as e:
             print(f"wystapil blad w receiveHTTPREAD treść {e}")
             traceback.print_exc()
-    #def receiveHTTPREAD_ReadFile(self, till=b'\n', count=2, counter=0):
-
+        cmd = "AT +HTTPTERM"
+        logging.debug(f"{cmd}")
+        self._send_cmd(cmd)
